b0acecb40a8fdb0086a6a4c377955229940b1969
Yael SG <yaeldh8@gmail.com>
2024-02-14 19:48:47 -0600

    V-1.0 auth token

--- controllers/tasksControllers.js
+++ controllers/tasksControllers.js
@@ -5,59 +5,69 @@ let Tasks = [
     (new Task(2, "task 2", "second task to display"))
 ];
 
+//Devuelve todas las tareas
 function getAllTasks() {
     return Tasks
 }
 
+//Crea una nueva tarea
 function createTask(tittle, description){
     const newTask = new Task(Tasks.length+1, tittle, description)
     Tasks.push(newTask)
     return newTask;
 }
 
+//Devuelve una tarea en específico
 function getSpecificTask(id){
-    return Tasks[id-1]
+    return Tasks[id-1];
 }
 
-function getFirstTask(){
-    return Tasks[0]
-}
-
-function getLatestTask(){
-    return Tasks[Tasks.length-1]
-}
-
+//Actualiza las tareas
 function updateTask(id, tiitle, description, done){
     Tasks.at(id-1).renew(tiitle, description, done);
     return Tasks[id-1]
 }
 
+//Elimina una tarea 
 function killTask(id){
     Tasks.splice(id-1, 1)
     return Tasks
 }
 
-function totalTasks(){
+//Devuelve el total de tareas
+function getTotalTasks(){
     return Tasks.length
 }
 
-function tasksDone(){
-
+//Devuelve las tareas completadas
+function getTasksDone(){
+    let tasksDone = [];
+    Tasks.forEach(function (T) {
+        if(T.done){
+            tasksDone.push(T);
+        }
+    })
+    return tasksDone;
 }
 
-function tasksYetToDone(){
-
+//Devuelve las tareas pendientes
+function getTasksNotDone(){
+    let tasksNotDone = [];
+    Tasks.forEach(function(T) {
+        if(!T.done){
+            tasksNotDone.push(T);
+        }
+    })
+    return tasksNotDone;
 }
 
 module.exports = {
     getAllTasks,
     createTask,
     getSpecificTask,
-    getFirstTask,
-    getLatestTask,
     updateTask,
     killTask,
-    totalTasks,
-    tasksDone,
-    tasksYetToDone
+    getTotalTasks,
+    getTasksDone,
+    getTasksNotDone
 }
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,40 +4,103 @@ const taskController = require('../controllers/tasksControllers');
 const authUtils = require('../middleware/authUtilities');
 const tokenAuth = require('../middleware/authMiddleware');
 
-function authentication(req, res, next) {
-    const Token = req.headers.authorization;
-    if (!Token){
-        return res.status(401).json({error: "Unauthorized"});
-    }
-    const decodedToken = authUtils.verifyToken(Token);
-    if(!decodedToken){
-        return res.status(401).json({error: "Unauthorized"});
-    }
-    res.user = decodedToken;
-    next();
-}
 
 router.get("/", tokenAuth, (req,res) => {
+    try{
     res.json(taskController.getAllTasks());
+    } catch (error){
+        res.status(406);
+    }
 });
 
-router.get("/", authentication, (res,req) => {
-    const {id} = req.params;
-    res.json(taskController.getSpecificTask(id));
+router.get("/first", tokenAuth, (req,res) => {
+    try{
+    res.json(taskController.getSpecificTask(1));
+    } catch (error) {
+        res.status(406);
+    }
 });
 
-router.get("/", authentication, (req,res) => {
-    res.json(taskController.getFirstTask());
+router.get("/latest", tokenAuth, (req,res) => {
+    try {
+    res.json(taskController.getSpecificTask(taskController.getTotalTasks()));
+    } catch (error) {
+        res.status(406);
+    }
 });
 
-router.get("/", authentication, (req,res) => {
-    res.json(taskController.getLatestTask());
+router.get("/total", tokenAuth, (req,res) => {
+    try {
+    res.json({message: `Total tasks registered: ${taskController.getTotalTasks()}`});
+    } catch (error) {
+        res.status(406);
+    }
 });
 
-router.post("/", authentication, (req,res) => {
-    const {tiitle, description} = req.body;
-    const newTask = taskController.createTask(tiitle, description);
-    res.status(201).json(newTask);
+router.get("/done", tokenAuth, (req, res) => {
+    try {
+        let tasksDone = taskController.getTasksDone();
+        if(tasksDone.length === 0){
+            res.json({message: "All tasks are still not done"});
+        } else {
+            res.json(tasksDone);
+        }
+    } catch (error) {
+        res.status(406);
+    }
+})
+
+router.get("/notdone", tokenAuth, (req, res) => {
+    try{
+        let tasksNotDone = taskController.getTasksNotDone();
+        if(tasksNotDone.length === 0){
+            res.json({message: "All tasks are done"});
+        } else {
+            res.json(tasksNotDone);
+        }
+    }catch (error) {
+    res.status(406);
+    }
+})
+
+router.post("/", tokenAuth, (req,res) => {
+    try {
+        const {tiitle, description} = req.body;
+        const newTask = taskController.createTask(tiitle, description);
+        res.status(201).json(newTask);
+    } catch (error) {
+        res.status(406);
+    }
+});
+
+router.get("/:id", tokenAuth, (req,res) => {
+    try {
+        const {id} = req.params;
+        res.json(taskController.getSpecificTask(id));
+    } catch (error) {
+        res.status(406);
+    }
+});
+
+router.put("/:id", tokenAuth, (req,res) => {
+    try{
+        const {id} = req.params;
+        const {tittle, description, done} = req.body;
+        const updateTask = taskController.updateTask(id, tittle, description, done);
+        res.status(202).json(updateTask);
+    }catch (error) {
+        res.status(406);
+    }
+});
+
+router.delete("/:id", tokenAuth, (req,res) => {
+    try {
+        const {id} = req.params;
+        const deleteTask = taskController.killTask(id);
+        res.status(202).json(deleteTask);
+    } catch (error) {
+        res.status(406);
+    }
 });
 
 module.exports = router;
